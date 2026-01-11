11e439ad40a35a971623aae5eab8bd44922327b4
yibotongxue <161041627+yibotongxue@users.noreply.github.com>
2026-01-11 15:44:31 +0800

    add scripts

---
+++ eval/llava1.5_entropy.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+
+dataset_name='coco' # 'aokvqa' 'gqa'
+image_folder='/home/yzh/cs285/AGLA/pope_source/val2014' # '/workspace/data/gqa'
+for seed in 1 2 3
+do
+for type in 'random' 'popular' 'adversarial'
+do
+python run_llava.py \
+--question-file ../data/POPE/${dataset_name}/${dataset_name}_pope_${type}.json \
+--image-folder ${image_folder} \
+--answers-file ../output/llava_${dataset_name}_pope_${type}_answers_entropy_seed${seed}.jsonl \
+--use_agla \
+--alpha 2 \
+--beta 0.5 \
+--use-entropy \
+--seed $seed
+done
+done
